fe5ee105f025f8e0d60907ba48b0b5bf2c53693a
meninn <gabrielm.boff@gmail.com>
2024-06-01 23:40:49 -0300

    route entity tests

--- src/route.entity.spec.ts
+++ src/route.entity.spec.ts
@@ -1,3 +1,37 @@
+import { Route, TRouteProps } from "./route.entity";
+
 describe("Route Tests", () => {
-  test("constructor", () => {});
+  test("constructor", () => {
+    let routeProps: TRouteProps = {
+      title: "route 1",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+    };
+
+    let route = new Route(routeProps);
+
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [],
+    });
+
+    routeProps = {
+      title: "route 1",
+      startPosition: { lat: 0, lng: 1 },
+      endPosition: { lat: 2, lng: 3 },
+      points: [
+        {
+          lat: 10,
+          lng: 14,
+        },
+      ],
+    };
+
+    route = new Route(routeProps);
+
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [{ lat: 10, lng: 14 }],
+    });
+  });
 });
